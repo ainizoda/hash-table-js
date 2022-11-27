bdfa9cd37d5d5dc49a96cd2768e5d837cccbc6ea
unknown <ainizoda@gmail.com>
2022-11-27 05:14:25 -0800

    minor fix

--- examples/non_string_key.js
+++ examples/non_string_key.js
@@ -3,6 +3,6 @@ import { Hash } from "../src/index.js";
 const fruitPrices = new Hash();
 const key = new Promise(() => {});
 
-fruitPrices.set(key, 25);
+fruitPrices.set(key, 25); // Error: Type of hash key cannot be 'promise'
 
-console.log(fruitPrices.get(key)); // Error: Type of hash key cannot be 'promise'
+console.log(fruitPrices.get(key));
